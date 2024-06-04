dc6ca19e23b88377bd1360a3776bf1169e018015
Vainovska <traynoev@gmail.com>
2024-06-04 21:08:58 +0300

    practic10

---
+++ src/route/auth.js
@@ -0,0 +1,36 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+const { User } = require('../class/user')
+
+// ================================================================
+router.get('/signup', function (req, res) {
+  return res.render('signup', {
+    name: 'signup',
+    component: [
+      'back-button',
+      'field',
+      'field-password',
+      'field-checkbox',
+      'field-select',
+    ],
+    title: 'Signup page',
+    data: {
+      role: [
+        { value: User.USER_ROLE.USER, text: 'Користувач' },
+        {
+          value: User.USER_ROLE.ADMIN,
+          text: 'Адміністратор',
+        },
+        {
+          value: User.USER_ROLE.DEVELOPER,
+          text: 'Розробник',
+        },
+      ],
+    },
+  })
+})
+// Підключаємо роутер до бек-енду
+module.exports = router
